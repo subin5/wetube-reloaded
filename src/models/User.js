ae3c49f994581edda2dbc5f208c39698d7b03a3e
subin5 <70627533+subin5@users.noreply.github.com>
2021-11-15 23:48:56 +0900

    3 week

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(){
     this.password = await bcrypt.hash(this.password, 5)
-})
+});
 
 const User = mongoose.model('User', userSchema);
 export default User;
