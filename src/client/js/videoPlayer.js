3334a67426993ec45adfa18e0becc1ac225848dc
subin5 <osb0408@ajou.ac.kr>
2022-01-24 18:17:55 +0900

    11.6 Timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 // global value
 let volumeValue = 0.5;
@@ -43,10 +44,17 @@ const formatTime = (seconds) =>
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
     currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimeLineChange = (event) => {
+    const { target: { value }, } = event;
+    video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -54,3 +62,4 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeLineChange);
