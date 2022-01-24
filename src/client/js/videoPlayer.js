bdd716b6a33a6f2cdf604b3ae8b083331732c614
subin5 <osb0408@ajou.ac.kr>
2022-01-24 18:01:32 +0900

    11.4 Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
 
 // global value
 let volumeValue = 0.5;
@@ -35,8 +36,18 @@ const handleVolumeChange = (event) => {
     }
     volumeValue = value;
     video.volume = value;
-}
+};
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+    currenTime.innerText = Math.floor(video.currentTime);
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
