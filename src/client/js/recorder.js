e559264333f76256a04aa952c3f3bd3275782727
subin5 <osb0408@ajou.ac.kr>
2022-01-25 16:18:51 +0900

    13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,30 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+};
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
     });
     video.srcObject = stream;
     video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
