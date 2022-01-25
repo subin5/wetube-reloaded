350357f21fdb4de3823d5152e089e20ce1ac2984
subin5 <osb0408@ajou.ac.kr>
2022-01-25 14:10:21 +0900

    12.2 Conclusions

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,7 +1,7 @@
 import express from "express"; 
 import { registerView } from "../controllers/VideoController";
 
-const apiRouter = express.Router;
+const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 
