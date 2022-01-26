a2c3e413b2100a2e8c83a8adda4e03e5e510e40f
subin5 <osb0408@ajou.ac.kr>
2022-01-26 12:57:17 +0900

    14.5 not yet

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -7,25 +7,38 @@ let stream;
 let recorder;
 let videoFile;
 
+const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg",
+}
+
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+}
 
 const handleDownload = () => {
     const ffmpeg = createFfmpeg({ log: true });
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", "files.input", await fetchFile(videoFile));
 
-    await ffmpeg.run("-i", recording.webm, "-r", "60", "output.mp4");
+    await ffmpeg.run("-i", files.input, "-r", "60", "files.output");
 
     await ffmpeg.run(
         "-i",
-        "recording.webm",
+        "files.input",
         "-ss", "00:00:01",
         -"frames:v",
         "1",
-        "thumbnail.jpg"
+        "files.thumb"
     );
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const mp4File = ffmpeg.FS("readFile", "files.output");
     const thumbFile = ffmpeg.FS("readFile", "thumnail.jpg");
 
     const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
@@ -34,17 +47,9 @@ const handleDownload = () => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
-    const a = document.createElement("a");
-    a.href = mp4Url;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
+    downloadFile(mp4Url, "MyRecording.mp4");
+    downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-    const thumbA = document.createElement("a");
-    thumbA.href = thumbUrl;
-    thumbA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
 };
 
 const handleStop = () => {
